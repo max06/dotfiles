2ca8ead62eaf6eeb23e1e9b712423e9b5addf73c
max06 <max06.net@outlook.com>
2020-09-22 19:51:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 # Dotfile install script
 
+# Basic packages
 apt-get install -y vim fish
+
+# Oh my fish
 curl -L https://get.oh-my.fish > /tmp/install
 fish /tmp/install --verbose --noninteractive
+
+# Powerline
+pip3 install powerline-status
+cp -r /usr/local/lib/python3.8/site-packages/powerline/config-files /root/.config/powerline
+fish -c "omf install powerline"
+
+chsh /usr/bin/fish
