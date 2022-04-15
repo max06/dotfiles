362f0fb7512eba1c8c03e0b038f033d72235ea1e
Florian Müller <max06.net@outlook.com>
2022-04-15 17:06:20 +0200

    Fixes

--- config/fish/conf.d/powerline.fish
+++ config/fish/conf.d/powerline.fish
@@ -1,4 +1,4 @@
 set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
 
 powerline-daemon -q
-fish_key_bindings=fish_default_key_bindings powerline-setup
+env fish_key_bindings=fish_default_key_bindings powerline-setup
