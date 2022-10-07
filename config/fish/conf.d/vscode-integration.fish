7c1d0bf986c6e8ae5ff3dcbe7ccce50e1351ec5b
max06 <max06.net@outlook.com>
2022-10-07 12:00:09 +0000

    Finalize vscode integration

--- config/fish/conf.d/vscode-integration.fish
+++ config/fish/conf.d/vscode-integration.fish
@@ -1,6 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     string match -q "$TERM_PROGRAM" "vscode"
-    and . /vscode/vscode-server/bin/linux-x64/74b1f979648cc44d385a2286793c226e611f59e7/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish
-    # (code --locate-shell-integration-path fish)
+    and . (code --locate-shell-integration-path fish)
 end
